8b9d36f94e0b972254f2a1c541fb9b2f57683915
Yameen Munir <74267033+YameenMunir@users.noreply.github.com>
2024-10-17 11:47:13 +0100

    Update BoardDemo.java

--- Battleships/src/Battleships/BoardDemo.java
+++ Battleships/src/Battleships/BoardDemo.java
@@ -2,8 +2,10 @@ package Battleships;
 
 public class BoardDemo
 {
+
     public static void main(String[] args)
     {
+
         //Creating a 10x10 Board
         Board b1 = new Board(10,10);
 
@@ -25,12 +27,12 @@ public class BoardDemo
         System.out.println(b1);
         System.out.println();
 
-        System.out.println("Bombing square x=2, y=0");
+        System.out.println("Bombing square x=2, y=1");
         b1.dropBomb(2, 1);
         System.out.println(b1);
         System.out.println();
 
-        System.out.println("Bombing square x=2, y=0");
+        System.out.println("Bombing square x=2, y=2");
         b1.dropBomb(2, 2);
         System.out.println(b1);
         System.out.println();
